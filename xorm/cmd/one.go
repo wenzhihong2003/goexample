866e57755108f93b40b88876d32e6fc9cbcee430
tomwen <wen.zhihong@myquant.cn>
2018-04-14 13:35:44 +0800

    第一个连接mysql的例子

--- xorm/cmd/one.go
+++ xorm/cmd/one.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/go-xorm/xorm"
+	"fmt"
 )
 
 var engine *xorm.Engine
@@ -11,5 +13,7 @@ func main() {
 	engine, err = xorm.NewEngine("mysql", "root:root@127.0.0.1:3307/apiserv?charset=utf8")
 	if err != nil {
 		panic(err)
+	}else {
+		fmt.Println("ok...........")
 	}
 }
